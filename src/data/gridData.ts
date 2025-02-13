cfaf9bba86be17120ccd89e5a05b4267476cac38
Kamal Feracho <kferacho3@users.noreply.github.com>
2025-02-13 00:34:24 -0500

    quickSelectImageUpdate

--- src/data/gridData.ts
+++ src/data/gridData.ts
@@ -13,19 +13,22 @@ export const permanentWidgets: Widget[] = [
     id: 101,
     title: "Products",
     route: "/explore/products",
-    image: "https://picsum.photos/id/101/500/500",
+    image:
+      "https://sunnyisland.s3.us-east-2.amazonaws.com/media/images/home/widgets/productsSIPS.webp",
   },
   {
     id: 102,
     title: "Scoville Scale",
     route: "/scoville",
-    image: "https://picsum.photos/id/102/500/500",
+    image:
+      "https://sunnyisland.s3.us-east-2.amazonaws.com/media/images/home/widgets/scovilleSIPS.webp",
   },
   {
     id: 103,
     title: "Foods all around!",
     route: "/explore/recipes",
-    image: "https://picsum.photos/id/103/500/500",
+    image:
+      "https://sunnyisland.s3.us-east-2.amazonaws.com/media/images/home/widgets/faaSIPS.webp",
   },
 ];
 
@@ -35,67 +38,77 @@ export const secondaryWidgetsData: Widget[] = [
     id: 1,
     title: "Shop",
     route: "/shop",
-    image: "https://picsum.photos/id/104/500/500",
+    image:
+      "https://sunnyisland.s3.us-east-2.amazonaws.com/media/images/home/widgets/shopSIPS.webp",
   },
   {
     id: 2,
     title: "About Us",
     route: "/explore/about",
-    image: "https://picsum.photos/id/105/500/500",
+    image:
+      "https://sunnyisland.s3.us-east-2.amazonaws.com/media/images/home/widgets/aboutUsSIPS.webp",
   },
   {
     id: 3,
     title: "Contact",
     route: "/contact/inquiries",
-    image: "https://picsum.photos/id/106/500/500",
+    image:
+      "https://sunnyisland.s3.us-east-2.amazonaws.com/media/images/home/widgets/contactSIPS.webp",
   },
   {
     id: 4,
     title: "Blog",
     route: "/explore/blog",
-    image: "https://picsum.photos/id/107/500/500",
+    image:
+      "https://sunnyisland.s3.us-east-2.amazonaws.com/media/images/home/widgets/blogSIPS.webp",
   },
   {
     id: 5,
     title: "Events",
     route: "/explore/events",
-    image: "https://picsum.photos/id/108/500/500",
+    image: "https://picsum.photos/id/108/500/500", // No matching S3 image provided
   },
   {
     id: 6,
     title: "Locations",
     route: "/explore/locations",
-    image: "https://picsum.photos/id/109/500/500",
+    image:
+      "https://sunnyisland.s3.us-east-2.amazonaws.com/media/images/home/widgets/locationsSIPS.webp",
   },
   {
     id: 7,
     title: "FAQ",
     route: "/contact/FAQs",
-    image: "https://picsum.photos/id/110/500/500",
+    image:
+      "https://sunnyisland.s3.us-east-2.amazonaws.com/media/images/home/widgets/faqSIPS.webp",
   },
   {
     id: 8,
     title: "Testimonials",
     route: "/testimonials",
-    image: "https://picsum.photos/id/111/500/500",
+    image:
+      "https://sunnyisland.s3.us-east-2.amazonaws.com/media/images/home/widgets/testimonialSIPS.webp",
   },
   {
     id: 9,
     title: "Careers",
     route: "/testimonials",
-    image: "https://picsum.photos/id/112/500/500",
+    image:
+      "https://sunnyisland.s3.us-east-2.amazonaws.com/media/images/home/widgets/careersSIPS.webp",
   },
   {
     id: 10,
     title: "Support Us",
     route: "/contact/supportUs",
-    image: "https://picsum.photos/id/113/500/500",
+    image:
+      "https://sunnyisland.s3.us-east-2.amazonaws.com/media/images/home/widgets/supportSIPS.webp",
   },
   {
     id: 11,
     title: "Questions?",
     route: "/contact/inquiries",
-    image: "https://picsum.photos/id/114/500/500",
+    image:
+      "https://sunnyisland.s3.us-east-2.amazonaws.com/media/images/home/widgets/inquiriesSIPS.webp",
   },
 ];
 
